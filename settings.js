bc82844fd87d24cba7a1e353ddce6b6c81936fcb
botdvinu <109034214+botdvinu@users.noreply.github.com>
2022-07-10 21:24:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
+global.alivemsg = `My name is BOTVINU🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+380944814219'] //Owner number in aive msg
